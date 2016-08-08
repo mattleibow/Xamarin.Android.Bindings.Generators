f711bdc7753db8e8fc74824fc5cfc8b65b285128
Matthew Leibowitz <mattleibow@live.com>
2016-08-09 01:15:02 +0200

    Added the source for a test Android jar

---
+++ src/AndroidTestLibrary/app/src/main/java/com/example/androidtestlibrary/MyExample.java
@@ -0,0 +1,27 @@
+package com.example.androidtestlibrary;
+
+public class MyExample {
+    public void testValid(String text) {
+
+    }
+
+    public void testValidMultiple(String text, String message) {
+
+    }
+
+    public void testKeyword(String string) {
+
+    }
+
+    public void testContextualKeyword(String async) {
+
+    }
+
+    public void testEmpty(String p0) {
+
+    }
+
+    public void testEmptyMultiple(String p0, String p1) {
+
+    }
+}
